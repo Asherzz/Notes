83439c7cc95e672dbac35bdaf09d4168f36fcca1
ash <team.my@mail.ru>
2021-12-17 01:57:27 +0300

    sql

--- Notes/Notes.Android/MainActivity.cs
+++ Notes/Notes.Android/MainActivity.cs
@@ -17,7 +17,8 @@ namespace Notes.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-            Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 0, 0, 0));
+            Window.SetStatusBarColor(Android.Graphics.Color.Black);
+            
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
